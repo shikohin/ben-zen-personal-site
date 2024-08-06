d1972b13093b67a71f72b6380ac70ae494b7e454
Benjamin Chapin <benjamin.n.chapin@gmail.com>
2024-08-06 14:32:41 -0500

    content update

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div style="text-align:right">
 		<h2>About</h2>
-		<p>Benjamin Chapin is a multi-faceted, multi-disciplinary artist, nerd, programmer and humanitarian. Ben resides in Chicago, Illinois. Ben is a connoisseur of nature, the arts, food, and science. </p>
+		<p>Benjamin Chapin is a multi-faceted, multi-disciplinary artist and developergit. Ben resides in Chicago, Illinois. Ben is a connoisseur of nature, the arts, food, and science. </p>
 	</div>
 </template>
