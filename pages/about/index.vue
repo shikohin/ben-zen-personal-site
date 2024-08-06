7df1bf5c35917a5c33bfdecaf68b6bdefcebb236
Benjamin Chapin <benjamin.n.chapin@gmail.com>
2024-08-06 14:23:14 -0500

    testing commit
    Co-authored-by: Kris <KrisGui@users.noreply.github.com>

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div style="text-align:right">
 		<h2>About</h2>
-		<p>Benjamin Chapin is a multi-faceted artist, nerd, programmer and humanitarian.</p>
+		<p>Benjamin Chapin is a multi-faceted, multi-disciplary artist, nerd, programmer and humanitarian.</p>
 	</div>
 </template>
